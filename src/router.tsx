d4b4b47d6197e2deb54dfa9713866d9079f0eb68
DxxxHxx <donghun1163@gmail.com>
2024-04-29 14:30:18 +0900

    feat:router 회원가입,로그인 페이지 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import Home from './routes/Home';
+import SignIn from './routes/SignIn';
+import SignUp from './routes/SignUp';
 
 export default function Router() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
       </Route>
     </Routes>
   );
