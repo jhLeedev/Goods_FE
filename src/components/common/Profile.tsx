0253ab602201a82c9da85965abd510f6154d61de
JeongHyeon <jhlee9638@gmail.com>
2024-04-30 01:30:14 +0900

    Feat: 사용자 정보 불러오기

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -1,34 +1,63 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
 export default function Profile() {
+  const getProfileInfo = () => {
+    return axios.get('http://localhost:5173/profile').then((res) => res.data);
+  };
+
+  const result = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfileInfo,
+  });
+
   return (
     <div className='w-full max-w-md h-36 mx-auto md:flex-auto md:m-0'>
       <div className='card card-side h-full pb-8 md:pb-0'>
         <div className='avatar'>
           <div className='w-28 rounded-xl mr-4 md:w-36 md:mr-8'>
-            <img
-              src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg'
-              alt='profile_image'
-            />
+            <img src={result.data.profile_image} alt='profile_image' />
           </div>
         </div>
         <div className='w-full flex flex-col items-start justify-around'>
           <div className='flex items-center'>
-            <h2 className='card-title mr-2'>닉네임</h2>
+            <h2 className='card-title mr-2'>{result.data.username}</h2>
             <div className='rating rating-sm'>
-              <input type='radio' name='rating-6' className='mask mask-star-2 bg-orange-400' />
-              <input
-                type='radio'
-                name='rating-6'
-                className='mask mask-star-2 bg-orange-400'
-                checked
-              />
-              <input type='radio' name='rating-6' className='mask mask-star-2 bg-orange-400' />
-              <input type='radio' name='rating-6' className='mask mask-star-2 bg-orange-400' />
-              <input type='radio' name='rating-6' className='mask mask-star-2 bg-orange-400' />
+              {[...Array(Math.floor(result.data.star))].map((i) => (
+                <svg
+                  key={i}
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='16'
+                  height='16'
+                  fill='rgb(251 146 60)'
+                  className='bi bi-star-fill'
+                  viewBox='0 0 16 16'
+                >
+                  <path d='M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z' />
+                </svg>
+              ))}
+              {[...Array(5 - Math.floor(result.data.star))].map((i) => (
+                <svg
+                  key={i}
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='16'
+                  height='16'
+                  fill='rgb(254 233 216)'
+                  className='bi bi-star-fill'
+                  viewBox='0 0 16 16'
+                >
+                  <path d='M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z' />
+                </svg>
+              ))}
             </div>
           </div>
           <div className='card-actions justify-end'>
-            <div className='badge badge-secondary badge-outline md:badge-lg'>판매왕</div>
-            <div className='badge badge-primary badge-outline md:badge-lg'>매너왕</div>
+            {result.data.sell_badge && (
+              <div className='badge badge-secondary badge-outline md:badge-lg'>판매왕</div>
+            )}
+            {result.data.manner_badge && (
+              <div className='badge badge-primary badge-outline md:badge-lg'>매너왕</div>
+            )}
           </div>
         </div>
       </div>
