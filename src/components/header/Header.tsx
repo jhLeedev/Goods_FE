b711099c5ca1a8cf7d13b121c4c7a5f367761ff4
DxxxHxx <donghun1163@gmail.com>
2024-05-11 13:33:10 +0900

    Refactor: header관련 컴포넌트 파일구조 변경

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,10 @@
 import { useRecoilValue } from 'recoil';
-import { isLoggedInState } from '../../store/atom';
-import Drawer from './Drawer';
+import Drawer from '../common/Drawer';
 import { useNavigate, useMatch, Link } from 'react-router-dom';
 import React from 'react';
 import logo from '../../assets/logo.webp';
+import { isLoggedInState } from '../../store/atom';
+import SearchBar from './SearchBar';
 
 export default function Header() {
   const isLoggedIn = useRecoilValue(isLoggedInState);
@@ -24,24 +25,7 @@ export default function Header() {
           <img src={logo} alt='logo img' className='w-8 h-8 md:w-12 md:h-12' />
         </div>
       </Link>
-      <label
-        htmlFor='searchInput'
-        className='flex items-center w-1/2 gap-2 md:w-1/3 input rounded-3xl input-bordered input-sm md:input-md'
-      >
-        <input id='searchInput' type='text' className='grow' placeholder='Search' />
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 16 16'
-          fill='currentColor'
-          className='w-5 h-5 text-black'
-        >
-          <path
-            fillRule='evenodd'
-            d='M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z'
-            clipRule='evenodd'
-          />
-        </svg>
-      </label>
+      <SearchBar />
       {isLoggedIn ? (
         <Drawer /> // 사이드바 (로그인 시)
       ) : (
