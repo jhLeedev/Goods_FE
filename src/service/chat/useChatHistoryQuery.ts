f34bfd3264123d3e3b71214247f6a5dca0c744af
DxxxHxx <donghun1163@gmail.com>
2024-05-18 13:01:10 +0900

    Feat: 채팅기록 목데이터,핸들러,유틸 함수 작성

---
+++ src/service/chat/useChatHistoryQuery.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { IChatHistoryData } from '../../types/interface';
+
+export const useChatHistoryQuery = (roomId: number) => {
+  const { data, isLoading } = useQuery<IChatHistoryData>({
+    queryKey: ['chatHistory', `${roomId}`],
+    queryFn: async () => (await axios.get(`/api/chat/${roomId}`)).data,
+  });
+  return { data, isLoading };
+};
